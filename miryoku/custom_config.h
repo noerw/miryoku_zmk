9c98ffe438d79869deb4578748ce66f9762ac04b
Norwin <git@nroo.de>
2024-04-06 01:42:48 +0200

    fixup! proper thumb key tap keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,7 +9,16 @@
 // - escape on outer left pinky
 // - swap space & backspace (tap only)
 // - rotate DEL,ESC,RET
-#define MIRYOKU_LAYERMAPPING_DEFAULT( \
+
+#define C32 U_LT(U_MEDIA,DEL)
+#define C33 U_LT(U_NAV,BSPC)
+#define C34 U_LT(U_MOUSE,TAB)
+
+#define C35 U_LT(U_SYM,ESC)
+#define C36 U_LT(U_NUM,SPACE)
+#define C37 U_LT(U_FUN, RET)
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
          K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
          K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
          K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -18,5 +27,6 @@
     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 &kp ESC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
     XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-U_LT(U_MEDIA,DEL) U_LT(U_NAV,BSPC) U_LT(U_MOUSE,TAB)       U_LT(U_SYM,ESC)  U_LT(U_NUM,SPACE) U_LT(U_FUN, RET)
+                   C32  C33  C34       C35  C36  C37 
 
+#define MIRYOKU_LAYERMAPPING_EXTRA MIRYOKU_LAYERMAPPING_BASE
