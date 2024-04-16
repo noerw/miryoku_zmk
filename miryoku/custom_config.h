dc30bab22dffd98606b8f0904c609333ccab333c
Norwin <git@nroo.de>
2024-04-16 23:45:10 +0200

    add outer column bindings

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,18 +5,26 @@
 
 //#define MIRYOKU_LAYOUTMAPPING_CORNE( \
 //#if defined (MIRYOKU_KEYBOARD_CORNE)
-// changes (for keyboardio m100 muscle memory):
-// - escape on outer left pinky
-// - swap space & backspace (tap only)
-// - rotate DEL,ESC,RET
 
-#define C32 U_LT(U_MEDIA,DEL)
-#define C33 U_LT(U_NAV,BSPC)
-#define C34 U_LT(U_MOUSE,TAB)
-
-#define C35 U_LT(U_SYM,ESC)
-#define C36 U_LT(U_NUM,SPACE)
+// thumb keys (reordered)
+#define C32 U_LT(U_MEDIA, DEL)
+#define C33 U_LT(U_NAV, BSPC)
+#define C34 U_LT(U_MOUSE, TAB)
+// pretty sure we dont need that, could use a tap shift?
+#define C35 U_LT(U_SYM, ESC)
+#define C36 U_LT(U_NUM, SPACE)
 #define C37 U_LT(U_FUN, RET)
+
+// right pinky: default en_us layout
+#define C19 U_MT(LGUI, SEMICOLON)
+
+// additional outer columns
+#define CL1 &kp BACKSLASH
+#define CL2 &kp ESCAPE
+#define CL3 XXX
+#define CR1 &kp MINUS
+#define CR2 &kp SINGLE_QUOTE
+#define CR3 XXX
 
 #define MIRYOKU_LAYERMAPPING_BASE( \
          K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -24,9 +32,9 @@
          K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
          N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-    XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp ESC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-    XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                   C32  C33  C34       C35  C36  C37 
+CL1  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  CR1 \
+CL2  K10  K11  K12  K13  K14       K15  K16  K17  K18  C19  CR2 \
+CL3  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  CR3 \
+               C32  C33  C34       C35  C36  C37
 
 #define MIRYOKU_LAYERMAPPING_EXTRA MIRYOKU_LAYERMAPPING_BASE
